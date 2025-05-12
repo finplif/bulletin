98f4cbae742b563e5c3d9542682bd3096e87f556
finplif <42929440+finplif@users.noreply.github.com>
2025-05-11 19:18:53 -0500

    Update utils.ts

--- app/events/utils.ts
+++ app/events/utils.ts
@@ -1,8 +1,19 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.https://bkzzipeqszjtleewsxzx.supabase.co!;
+const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrenppcGVxc3pqdGxlZXdzeHp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMDY4MzQsImV4cCI6MjA2MjU4MjgzNH0.orwVh0bWTiXXOjz4syDwQBcws_VNSe0UJKWnv6XEt_I!;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 export async function getEvents() {
-    const res = await fetch(
-      'https://api.sheetbest.com/sheets/18fc5c53-3ee1-44bf-a147-dda2473191fd',
-      { cache: 'no-store' }
-    );
-    const data = await res.json();
-    return data;
+  const { data, error } = await supabase
+    .from('events')
+    .select('*')
+    .order('date', { ascending: true });
+
+  if (error) {
+    console.error('Error fetching events:', error);
+    return [];
   }
+
+  return data;
+}
