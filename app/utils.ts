af292c4501635f32a86f1ce19c98b22f3a7db865
finplif <42929440+finplif@users.noreply.github.com>
2025-05-20 13:18:34 -0500

    Update utils.ts

--- app/utils.ts
+++ app/utils.ts
@@ -5,6 +5,14 @@ const supabaseUrl = 'https://bkzzipeqszjtleewsxzx.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrenppcGVxc3pqdGxlZXdzeHp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMDY4MzQsImV4cCI6MjA2MjU4MjgzNH0.orwVh0bWTiXXOjz4syDwQBcws_VNSe0UJKWnv6XEt_I';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+export interface VenueItem {
+  id: number;
+  name: string;
+  address: string;
+  hood: string;
+  slug: string;
+}
+
 function slugify(text: string): string {
   return text.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)+/g, '');
 }
