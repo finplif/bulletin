022af76ddcd15e3790beca3d5b8060a14fcc3052
finplif <42929440+finplif@users.noreply.github.com>
2025-05-20 15:30:44 -0500

    Update utils.ts

--- app/utils.ts
+++ app/utils.ts
@@ -21,23 +21,23 @@ export async function getEvents() {
 const { data, error } = await supabase
   .from('events')
   .select(`
-  id,
-  title,
-  date,
-  time_start,
-  time_end,
-  type,
-  descr,
-  link,
-  slug,
-  venue_id,
-  venues (
-    name,
-    address,
-    hood,
-    slug
-  )
-`)
+    id,
+    title,
+    date,
+    time_start,
+    time_end,
+    type,
+    descr,
+    link,
+    slug,
+    venue_id,
+    venue:venues!venue_id (
+      name,
+      address,
+      hood,
+      slug
+    )
+  `)
 
   if (error) {
     console.error('Error fetching events:', error);
@@ -60,9 +60,9 @@ const { data, error } = await supabase
     descr: event.descr,
     link: event.link,
     slug: event.slug,
-    venue: event.venues && event.venues.name
-      ? event.venues
-      : { name: '', address: '', hood: '', slug: '' }
+    venue: event.venue && event.venue.name
+  ? event.venue
+  : { name: '', address: '', hood: '', slug: '' }
   }));
 }
 
