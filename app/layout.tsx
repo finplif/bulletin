8a9e3a67a2fd34b645227830f0edfff2680552db
finplif <42929440+finplif@users.noreply.github.com>
2025-05-19 22:08:14 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { DM_Sans } from 'next/font/google';
+import Header from './components/Header';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
