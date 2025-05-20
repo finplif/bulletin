97824da38ad7d775c948d7e8308d0e8d39543865
finplif <42929440+finplif@users.noreply.github.com>
2025-05-20 09:12:04 -0500

    Update utils.ts

--- app/utils.ts
+++ app/utils.ts
@@ -4,6 +4,10 @@ const supabaseUrl = 'https://bkzzipeqszjtleewsxzx.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrenppcGVxc3pqdGxlZXdzeHp4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwMDY4MzQsImV4cCI6MjA2MjU4MjgzNH0.orwVh0bWTiXXOjz4syDwQBcws_VNSe0UJKWnv6XEt_I';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+function slugify(text: string): string {
+  return text.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)+/g, '');
+}
+
 export async function getEvents() {
   const { data, error } = await supabase
     .from('events')
@@ -29,18 +33,18 @@ export async function getEvents() {
     return [];
   }
 
-return data.map((event: any) => ({
-  id: event.id,
-  title: event.title,
-  date: event.date,
-  time_start: event.time_start,
-  time_end: event.time_end,
-  type: event.type,
-  descr: event.descr,
-  link: event.link,
-  slug: event.slug,
-  venue: event.venue || { name: '', address: '', hood: '' }, 
-}));
+  return data.map((event: any) => ({
+    id: event.id,
+    title: event.title,
+    date: event.date,
+    time_start: event.time_start,
+    time_end: event.time_end,
+    type: event.type,
+    descr: event.descr,
+    link: event.link,
+    slug: event.slug || slugify(event.title),
+    venue: event.venues?.[0] || { name: '', address: '', hood: '' }, // <- FIXED HERE
+  }));
 }
 
 export async function getVenues() {
