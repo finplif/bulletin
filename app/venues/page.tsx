38a69249d25300542dc47a6579fb6ecf400b8ce7
finplif <42929440+finplif@users.noreply.github.com>
2025-05-21 19:51:17 -0500

    Update page.tsx

--- app/venues/page.tsx
+++ app/venues/page.tsx
@@ -33,6 +33,7 @@ export default async function Page() {
               >
                 {venue.name}
               </Link>
+              <p className="text-sm text-gray-600">{venue.hood}</p>
             </li>
           ))}
         </ul>
