1a6973f803a8ca74920a48199545036ae1e03171
finplif <42929440+finplif@users.noreply.github.com>
2025-06-28 17:56:07 +0200

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -19,7 +19,8 @@ export default function Header() {
         <Link href="/exhibitions" className={linkClass('/exhibitions')}>exhibitions</Link>
         <Link href="/galleries" className={linkClass('/galleries')}>galleries</Link>
         <div className="h-4 border-l border-gray-400 mx-2" />
-        <Link href="/about" className={linkClass('/galleries')}>about</Link>
+        <Link href="/about" className={linkClass('/about')}>about</Link>
+        <Link href="/contact" className={linkClass('/contact')}>contact</Link>
       </nav>
     </header>
   );
